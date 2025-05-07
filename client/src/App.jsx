f0613b44d4fed1a0fbbd3936a1ab52e4319a2870
aniruddhapai27 <aniruddhaanantpai@gmail.com>
2025-05-07 16:01:22 +0530

    Updated the App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ const App = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-9xl text-blue-500">
       MINI PROJECT
-      
       <p className="text-3xl text-gray-900">Deployed with git actions</p>
     </div>
   );
