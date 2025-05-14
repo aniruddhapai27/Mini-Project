854cebec4c0c4e19978d804fa01fd5cd3fe9a0db
aniruddhapai27 <aniruddhaanantpai@gmail.com>
2025-05-14 11:07:27 +0530

    Updated vite.config.js file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 const App = () => {
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get("/services/");
+        console.log(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchUser();
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center h-screen text-9xl text-blue-500">
       MINI PROJECT
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/services": {
+        target: "https://mini-project-fastapi-g9kp.onrender.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/services/, ""),
+      },
+    },
+  },
 });
