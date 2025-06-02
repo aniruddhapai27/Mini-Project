e62691f8f85f31507bd22bab960990b88bb76ee5
aniruddhapai27 <aniruddhaanantpai@gmail.com>
2025-06-02 11:44:46 +0530

    refactor: restructure routing in App component and wrap with BrowserRouter in main entry

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthLayout from "./components/AuthLayout";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import { Route, Router, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<AuthLayout />}>
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route
-            path="dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<AuthLayout />}>
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route
+          path="dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Route>
+    </Routes>
   );
 };
 
