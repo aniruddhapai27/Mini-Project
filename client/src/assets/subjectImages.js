3379a05630ab8b0f18420cd1312e6b75c3ee3085
Channu17 <channudambal2004@gmail.com>
2025-06-14 16:23:42 +0530

    feat: add subject images for various computer science topics

---
+++ client/src/assets/subjectImages.js
@@ -0,0 +1,8 @@
+export const subjectImages = {
+  'data structures': 'https://img.icons8.com/ios-filled/100/000000/data-configuration.png',
+  'operating systems': 'https://img.icons8.com/ios-filled/100/000000/windows8-settings.png',
+  'computer networks': 'https://img.icons8.com/ios-filled/100/000000/network.png',
+  'database management systems': 'https://img.icons8.com/ios-filled/100/000000/database.png',
+  'software engineering': 'https://img.icons8.com/ios-filled/100/000000/engineering.png',
+  'algorithm design and analysis': 'https://img.icons8.com/ios-filled/100/000000/flow-chart.png',
+};
