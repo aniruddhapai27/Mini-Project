20720053093a19f183e8600765a8955d80b547d8
Aniruddha Anant Pai <149802807+aniruddhapai27@users.noreply.github.com>
2025-06-26 21:57:20 +0530

    Update AuthLayout.jsx

--- client/src/components/AuthLayout.jsx
+++ client/src/components/AuthLayout.jsx
@@ -3,7 +3,6 @@ import Navbar from "./Navbar";
 
 const AuthLayout = () => {
   return (    <div className="min-h-screen bg-black relative overflow-hidden">
-     
       <div className="relative z-10">
         <Navbar />
         <main className="w-full">
