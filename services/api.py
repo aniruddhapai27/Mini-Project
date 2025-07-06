d9e646db640f4e6c9feef8589c0467d2d0751002
Aniruddha Anant Pai <149802807+aniruddhapai27@users.noreply.github.com>
2025-07-06 10:58:47 +0530

    added localhost:5173 for cors in services

--- services/api.py
+++ services/api.py
@@ -36,7 +36,7 @@ api = FastAPI(
 # Allow CORS for all origins    
 api.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://my-project.tech", "http://localhost:5000"],
+    allow_origins=["http://localhost:3000", "https://my-project.tech", "http://localhost:5000", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
